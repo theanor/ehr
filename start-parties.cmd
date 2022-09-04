02d353773902d01955efac994113b1931fbc1b46
theanor <robert.barbiaux@gmail.com>
2022-09-04 15:40:06 +0200

    fix sample start script

--- start-parties.cmd
+++ start-parties.cmd
@@ -1,6 +1,8 @@
-start java -jar service-registry\target\service-registry-2.2.5.RELEASE.jar 
+start java -jar service-registry\target\service-registry-1.0.0.jar 
 pause
-start java -jar parties-service\target\parties-service-0.0.1-SNAPSHOT.jar 
+start java -jar iam\iam-service\target\iam-service-1.0.0.jar 
 pause
-start java -jar api-gateway\target\api-gateway-2.2.5.RELEASE.jar 
+start java -jar api-gateway\target\api-gateway-1.0.0.jar
+pause
+start java -jar parties\parties-service\target\parties-service-1.0.0.jar 
 
